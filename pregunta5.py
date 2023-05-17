61ec586bc5211fc59a253f41082f534abc16766e
David Ruiz <david.ruizluque@usp.ceu.es>
2023-05-17 20:32:45 +0200

    Update pregunta5.py

---
+++ pregunta5.py
@@ -0,0 +1,15 @@
+""""
+PREGUNTA 5
+Dado un grafo no dirigido con personajes del MCU de la siguiente tabla:
+Implementar los algoritmos necesarios para resolver las siguientes tareas:
+
+    a. cada vértice debe almacenar el nombre de un personaje, las aristas representan la cantidad de episodios en los que aparecieron juntos ambos personajes que se relacionan;
+    
+    b. hallar el árbol de expansión máximo desde el vértice que contiene a Iron-Man, Thor y The Winter Soldier;
+    
+    c. determinar cuál es el número máximo de episodio que comparten dos personajes, e indicar todos los pares de personajes que coinciden con dicho número
+    
+    d. cargue todos los personajes de la tabla anterior
+    
+    e. indicar qué personajes aparecieron en nueve episodios de la saga.
+"""
