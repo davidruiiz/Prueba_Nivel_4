0b29b0e8ee8aca372c3287a38ba5c1670d4c3656
David Ruiz <david.ruizluque@usp.ceu.es>
2023-05-17 20:31:21 +0200

    Update pregunta4.py

---
+++ pregunta4.py
@@ -0,0 +1,8 @@
+""""
+PREGUNTA 4
+Nick Fury se encuentra en los cuarteles generales de S.H.I.E.L.D. y debe visitar a varios superhéroes para convencerlos de unirse para formar un grupo de vengadores, dado que es un asunto de suma importancia nos solicita implementar un algoritmo que permita determinar el recorrido de menor distancia (el menor posible, no importa que sea el óptimo) y terminar dicho recorrido de vuelta en los cuarteles (solo se puede pasar una vez por cada lugar).
+
+    a. Considere los siguientes superhéroes: S.H.I.E.L.D.
+    
+    b. las distancias entre la localización de cada superhéroe están cargadas en la siguiente matriz:
+"""
